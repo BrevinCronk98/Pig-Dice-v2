89a18f7afe2f2cf10e403c1d9a378018f992e757
unknown <macjay314@gmail.com>
2020-04-08 10:49:26 -0700

    Experiment with various back-end functions. One for dice roll press. One for 'hold' button press. One to keep track of score. And finally, one to reset the game when needed.

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,15 @@
 var userScores = {
     playerOne: 0,
-    playeTwo: 0,
+    playerTwo: 0,
 }
 
 var turnScores  = {
     currentTotal: 0,
-    curentRoll: 0,
+    currentRoll: 0,
     turnCount: 1,
+    playerOneTurn = true;
+    playerTwoTurn = false;
 }
-
-
 
 
 function diceRoll(){
@@ -17,34 +17,94 @@ function diceRoll(){
     const max = 7;
     const roll= Math.floor(Math.random()*(max- min)+ min)
     console.log(roll);
-}
-
-function playerOneScore() {
-    turnScores.turnCount++
-    turnScores.currentRoll = diceRoll();
-    // if
-
+    
+    
+    if (roll == 1){
+       $("#diceOne").show()
+    }else if(roll == 2){
+        
+        $("#diceTwo").show();
+        
+    }else if(roll == 3){
+       
+        $("#diceThree").show();
+        
+    }else if (roll == 4){
+        
+        $("#diceFour").show();
+    }else if (roll == 5){
+        
+        $("#diceFive").show();
+    }else if (roll == 6){
+        
+        $("#diceSix").show();
+    }else{
+        alert('bug splat');
+    }
+    return roll;
 }
     
+function turnEval(){
+    ($("#p1-hold").on("click") {
+        playerTwoTurn = true   
+    }
+    console.log(works)
+}
+
+function resetGame() {
+ turnScores.currentTotal = 0;
+ turnScores.currentRoll = 0;
+ turnScores.turnCount = 1;
+ userScores.playerTwo = 0;
+ userScores.playerOne = 0;
+}
 
 
+function addTurnTotal() {
+    turnScores.currentRoll = diceRoll();
+    if(turnScores.currentRoll != 1){
+        return turnScores.currentTotal += turnScores.currentRoll
+    } else {
+        gameUpdate();
+        return turnScores.currentRoll = 0;
+    }
+}
+
+function hold(playerOneTurn, playerTwoTurn){
+  gameUpdate();
+  if(playerOneTurn){
+      userScores.playerOne += turnScores.currentRoll;
+      console.log(userScores.playerOne)
+  } else if(playerTwoTurn) {
+      userScores.playerTwo += turnScores.currentRoll
+      console.log(userScores.playerOneTurn)
+      
+ }
 
 
+}
 
 $(document).ready(function(){
-    //  event.preventDefault();
 
-    $("#p1-dice").click(function(event){
-        event.preventDefault();
-        diceRoll();
-
-         $("#p1-roll").text(diceRoll);        
-       
-    })
-
-    
-
-
-
-
-
+  $("#p1-dice").click(function(event){
+    event.preventDefault();
+    addTurnTotal();
+    $("#p1-roll").text(diceRoll());
+  })
+  
+  
+  
+  
+  $("#p1-hold").click(function(event){
+    event.preventDefault();
+    hold();
+  })
+  $("#p2-dice").click(function(event){
+    event.preventDefault();
+    $("#p2-roll").text(diceRoll());
+  })
+  $("p2-hold").click(function(event){
+    event.preventDefault();
+    hold();
+  })
+})
