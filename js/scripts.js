49227ed37ce75ef050c45032ef9fe8f7aadd9439
unknown <macjay314@gmail.com>
2020-04-09 11:37:22 -0700

    Attempt to make an ai to battle the player. Create a while loop to keep track of turn.

--- js/scripts.js
+++ js/scripts.js
@@ -1,42 +1,42 @@
 var userScores = {
     playerOne: 0,
-    playerTwo: 0,
+    playerTwo:0,
 }
 
 var turnScores  = {
     currentTotal: 0,
     currentRoll: 0,
-    turnCount: 1,
+    playerTurn: true,
 }
 
 function turnCounter() {
-  turnScores.turnCount ++ ;
-  if(turnScores.turnCount % 2 == 0){
-      $("#p2roll").show();
-      $("#p1roll").hide();
-  } else {
+  if(turnScores.playerTurn == true){
       $("#p1roll").show();
       $("#p2roll").hide();
+  } else {
+      turnScores.playerTurn = false;
+      $("#p2roll").show();
+      $("#p1roll").hide();
   }
-  return turnScores.turnCount;
+
 }
 
 
 function resetGame() {
     turnScores.currentTotal = 0;
     turnScores.currentRoll = 0;
-    turnScores.turnCount = 1;
+    turnScores.playerTurn = true;
     userScores.playerTwo = 0;
     userScores.playerOne = 0;
 }
    
 
-function diceRoll(){
+function diceRoll() {
   const min = 1;
   const max = 7;
   const roll= Math.floor(Math.random()*(max - min) + min)
 
-  
+
   $("img").hide();
   switch (roll){
       case 1: {$("#diceOne").show()}
@@ -54,7 +54,11 @@ function diceRoll(){
   }
     return roll;
 }
-
+function aiDiceRoll() {
+  const min = 1;
+  const max = 7;
+  const roll= Math.floor(Math.random()*(max - min) + min)
+  }
 
 function addRoll() {
   turnScores.currentRoll = diceRoll();
@@ -68,7 +72,7 @@ function addRoll() {
 }
 
 function addTurnTotal(){
-  if(turnScores.turnCount % 2 != 0){
+  if(turnScores.playerTurn == true){
     userScores.playerOne += turnScores.currentTotal;
     turnScores.currentTotal = 0;
     if(userScores.playerOne >= 100){
@@ -82,48 +86,111 @@ function addTurnTotal(){
         $("#winner").text("Player Two Wins!!");
     }
   }
+  turnCounter();
 }
 
+// function easyAi(){
+//   const aiMove = Math.floor(Math.random() * 6) + 1;
+//     return aiMove;
+//   }
 
-$(document).ready(function(){
-    
-    $("#new").click(function(event){
-        event.preventDefault();
+function aiTurn(){
+  roll = diceRoll();
+  if (roll !== 1){
+    addRoll();
+    addTurnTotal();
+  } else turnCounter();
+}
+  
+$(document).ready(function(){    
+  $("#new").click(function(event){
+      event.preventDefault();
+      resetGame();
+      $("#p1score").text("Player One Score:" + userScores.playerOne);
+      $("#p2score").text("Player Two Score:" + userScores.playerTwo);
+      $("#roll").show();
+      $("#hold").show();
+      $("#new").hide();  
+      $("#hard-btn").hide();
+      $("#pig").show();
+     
+      console.log(turnScores.turnCount)
+  })
 
-        resetGame();
+  $("#roll").click(function(event){
+      event.preventDefault();
+      addRoll();
+      $("#rollCount").show();
+      $("#pig").show();
+      $("#p1score").text("Player One Score:" + userScores.playerOne);
+      $("#rollCount").text("Roll:" + turnScores.currentRoll)
+      $("#p2score").text("Player Two Score:" + userScores.playerTwo); 
+      console.log(turnScores.turnCount)    
+  })
 
-        $("#p1score").text("Player One Score:" + userScores.playerOne);
-        $("#p2score").text("Player Two Score:" + userScores.playerTwo);
-        $("#roll").show();
-        $("#hold").show();
-        $("#new").hide()  
-        console.log(turnScores.turnCount)
+  $("#hold").click(function(event){
+      event.preventDefault();
+      addTurnTotal()
+      $("#p1score").text("Player One Score:" + userScores.playerOne);
+      $("#p2score").text("Player Two Score:" + userScores.playerTwo);
+      $("#rollCount").hide();
+      $("#pig").show();
+      console.log(turnScores.turnCount)
     })
 
-
-$("#roll").click(function(event){
-    event.preventDefault();
-    addRoll();
-    $("#rollCount").show();
-
-    $("#p1score").text("Player One Score:" + userScores.playerOne);
-    $("#rollCount").text("Roll:" + turnScores.currentRoll)
-    $("#p2score").text("Player Two Score:" + userScores.playerTwo);
- 
-    console.log(turnScores.turnCount)
     
-
-})
-
-$("#hold").click(function(event){
+  $("#hard-btn").click(function(event){
     event.preventDefault();
+    $("#aiRoll").show();
+    $("#aiHold").show();
+    $("#new").hide();
+    $("#hard-btn").hide();
+    $("#pig").show();
+    while(userScores.playerOne < 100 && userScores.playerTwo < 100){
+      $("#p1score").text("Player One Score:" + userScores.playerOne);
+      $("#p2score").text("Player Two Score:" + userScores.playerTwo);
+      if(playerTurn === false){
+        aiTurn();
+      } else{
+        $("#aiRoll").click(addRoll());
+        $("#aiHold").click(addTurnTotal());
+        // console.log("hello");
+      }
+  }
+  })
 
-    addTurnTotal()
-    $("#p1score").text("Player One Score:" + userScores.playerOne);
-    $("#p2score").text("Player Two Score:" + userScores.playerTwo);
-    turnCounter();
-    $("#rollCount").hide();
-    console.log(turnScores.turnCount)
-})
 
+
+    
+    // $("#hard-btn").click(function(event){
+    //   event.preventDefault();
+    //   $("#aiRoll").show();
+    //   $("#aiHold").show();
+    //   $("#new").hide();
+    //   $("#hard-btn").hide();
+    //   $("#pig").show();
+    // })
+    
+    // $("#aiRoll").click(function(event){
+    //   event.preventDefault();
+    //   addRoll();
+    //   $("#rollCount").show();
+    //   $("#pig").show();
+    //   $("#p1score").text("Player One Score:" + userScores.playerOne);
+    //   $("#rollCount").text("Roll:" + turnScores.currentRoll)
+    //   $("#p2score").text("Player Two Score:" + userScores.playerTwo);
+    // })
+    
+    // $("#aiHold").click(function(event){
+    //   event.preventDefault();
+    //   // aiTurn();
+    //   turnCounter();
+    //   addTurnTotal();
+    //   aiRoll();
+    //   $("#pig").show();
+    //   $("#p1score").text("Player One Score:" + userScores.playerOne);
+    //   $("#p2score").text("Player Two Score:" + userScores.playerTwo);
+    //   console.log(turnScores.currentRoll);
+    //   console.log(turnScores.currentTotal);
+    // })
 })
